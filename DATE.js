335339f23dc88b6c4c953a10eee72a62d63e918f
mirali75 <hadiyamirali074@gmail.com>
2026-05-18 13:32:18 +0530

    Create DATE.js

---
+++ DATE.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="demo"></p>
+    <script>
+        const d=new Date();
+        document.getElementById("demo").innerHTML=d;
+    </script>
+      <!-- <script>
+        const d=new Date(2022, 6, 31, 10, 33, 30);
+        document.getElementById("demo").innerHTML=d;
+    </script>  -->
+</body>
+</html>
