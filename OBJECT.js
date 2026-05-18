77a2575b47166cfba7c61831776de79e98201065
mirali75 <hadiyamirali074@gmail.com>
2026-05-18 13:44:11 +0530

    Create OBJECT.js

---
+++ OBJECT.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+    //     var person = new Object();
+
+    //     person.firstname = "HADIYA";
+    //     person.lastname = "MIRALI";
+
+    //     person.sayhi = ()=>{
+    //         alert("WELCOME! "+ person.firstname +" "+person.lastname)
+    //    }
+    //    person.sayhi();
+
+       var person = { firstname : "HADIYA", lastname : "MIRALI", 
+       sayhi: function()
+       {
+            alert("WELCOME! "+ person.firstname +" "+person.lastname);
+       }
+    }
+       person.sayhi();
+    </script>
+    </script>
+</body>
+</html>
