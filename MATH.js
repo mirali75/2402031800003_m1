e07e73c5e12f60906244704cbcf10e9b9373cb26
mirali75 <hadiyamirali074@gmail.com>
2026-05-18 13:42:07 +0530

    Create MATH.js

---
+++ MATH.js
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script>
+        document.writeln(Math.E);
+        document.write("<br>");
+
+        var a=Math.pow(2,3);
+        document.writeln(a);
+        document.write("<br>");
+
+        var num=Math.random();
+        document.writeln(num);
+        document.write("<br>");
+
+        var num2=parseInt(1+(Math.random()*100));
+
+        document.writeln(num2);
+        document.write("<br>");
+
+        document.write(Math.ceil(2,7));
+        document.write("<br>");
+        document.write(Math.floor(2,7));
+        document.write("<br>");
+        document.write(Math.round(2,7));
+    </script>
+</body>
+</html>
