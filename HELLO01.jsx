6a3c030fbb096b44ae1df6e1bdfa01a9cd25008a
mirali75 <hadiyamirali074@gmail.com>
2026-06-01 10:28:07 +0530

    Create HELLO01.jsx

---
+++ HELLO01.jsx
@@ -0,0 +1,10 @@
+function Hello01(props){
+    return( 
+    <>
+    <h2>Hello, {props.name}</h2>
+    <h2>your are {props.age} years old.</h2>
+    <h2>you are from {props.city}.</h2>
+    </>
+    )
+}
+export default Hello01
